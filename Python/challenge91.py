6065ce13535dba4dd66a950aca67b347991cd0d9
Liam Keene <liam.keene@gmail.com>
2013-06-13 14:11:31 +1000

    Modified Challenge 91 to get 100%
     modified:   Python/challenge91.py - output must be formatted to 3 decimal
                    places (not specified in the challenge)

--- Python/challenge91.py
+++ Python/challenge91.py
@@ -5,6 +5,6 @@ test_cases = open(sys.argv[1], 'r')
 for test in test_cases:
     li = [float(elem) for elem in test.split()]
     li.sort()
-    print ' '.join(['{}'.format(elem) for elem in li])
+    print ' '.join(['{:.3f}'.format(elem) for elem in li])
 
 test_cases.close()
